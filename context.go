7003c319922bc42fe2f6d2c5b1eef46da1b64569
Eric Chu <eytchu@gmail.com>
2015-05-25 16:16:12 -0700

    simple context for plumb

---
+++ context.go
@@ -0,0 +1,15 @@
+package plumb
+
+type Field struct {
+  Name string
+  Description string `yaml:",omitempty"`
+  Type string
+}
+
+type PlumbContext struct {
+  Language string
+  Name string
+  Inputs []Field `yaml:",flow"`
+  Outputs []Field `yaml:",flow"`
+  Env []string `yaml:",flow,omitempty"`
+}
