4cff92332623a0415233b54970c7f9c6c63810d8
Eric Chu <eytchu@gmail.com>
2015-05-25 16:16:27 -0700

    added bundle command (for python-specific projects)

---
+++ bundle.go
@@ -0,0 +1,188 @@
+package plumb
+
+import (
+  "log"
+  "io/ioutil"
+  "fmt"
+  "gopkg.in/yaml.v2"
+  "os"
+  "text/template"
+  "os/exec"
+  "bufio"
+  "io"
+)
+
+type templateContext struct {
+  Wrapper string
+  Plumb *PlumbContext
+}
+
+const bundleConfig = ".plumb.yml"
+
+// A Dockerfile template for python bundles. We'll make sure this is
+// a standalone file in the future, so we can add different language
+// types.
+const dockerfileTemplate = `
+FROM python:2-onbuild
+RUN pip install bottle
+EXPOSE 9456
+{{ range .Plumb.Env }}
+ENV {{ . }}
+{{ end }}
+CMD ["python", "{{ .Wrapper }}"]
+`
+
+const wrapperTemplate = `
+import {{ .Plumb.Name }}
+from bottle import post, route, run, request, HTTPResponse
+
+__INFO = {'bundle': '{{ .Plumb.Name }}',
+  'inputs': {
+    {{ range .Plumb.Inputs }}'{{ .Name }}': '{{ .Description }}',{{ end }}
+  },
+  'outputs': {
+    {{ range .Plumb.Outputs }}'{{ .Name }}': '{{ .Description }}',{{ end }}
+  }
+}
+
+@route('/info')
+def info():
+    return __INFO
+
+@post('/')
+def index():
+    data = request.json
+
+    # validate input data
+    if data is None:
+        raise HTTPResponse(
+            body = "No JSON data received; did you forget to set 'Content-Type: application/json'?",
+            status = 400
+        )
+    {{ range .Plumb.Inputs }}
+    if not '{{ .Name }}' in data:
+        raise HTTPResponse(
+            body = "Missing required '{{ .Name }}' field in JSON data.",
+            status = 400
+        )
+    {{ end }}
+    # run the enhancer
+    output = {{ .Plumb.Name }}.run(data)
+
+    # validate output data
+    {{ range .Plumb.Outputs }}
+    if not '{{ .Name }}' in output:
+        raise HTTPResponse(
+            body = "Unexpected output; missing '{{ .Name }}' field.",
+            status = 501
+        )
+    {{ end }}
+
+    return output
+
+run(host='0.0.0.0', port=9456)
+`
+
+func removeTempFile(f *os.File) {
+  filename := f.Name()
+  log.Printf(" |  Removing '%s'", filename)
+  err := os.RemoveAll(filename)
+  if err != nil {
+    log.Printf("    %v", err)
+  } else {
+    log.Printf("    Removed.")
+  }
+}
+
+func Bundle(path string) error {
+  ctx := PlumbContext{}
+  log.Printf("==> Creating bundle from '%s'", path)
+  defer log.Printf("<== Bundling complete.")
+
+  log.Printf(" |  Parsing bundle config.")
+  bytes, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", path, bundleConfig))
+  if err != nil {
+    return err
+  }
+  if err := yaml.Unmarshal(bytes, &ctx); err != nil {
+    return err
+  }
+  log.Printf("    %v", ctx)
+
+  log.Printf(" |  Making temp file for python wrapper")
+  wrapper, err := ioutil.TempFile(path, "plumb")
+  defer removeTempFile(wrapper)
+  if err != nil {
+    return err
+  }
+  log.Printf("    Created '%s'", wrapper.Name())
+
+  templateCtx := templateContext{
+    Wrapper: wrapper.Name(),
+    Plumb: &ctx,
+  }
+
+  log.Printf(" |  Writing wrapper.")
+  tmpl, err := template.New("wrapper").Parse(wrapperTemplate)
+  if err != nil {
+    return err
+  }
+  if err := tmpl.Execute(wrapper, templateCtx); err != nil {
+    return err
+  }
+  log.Printf("    Done.")
+
+  log.Printf(" |  Making temp file for Dockerfile")
+  dockerfile, err := ioutil.TempFile(path, "plumb")
+  defer removeTempFile(dockerfile)
+  if err != nil {
+    return err
+  }
+  log.Printf("    Created '%s'", dockerfile.Name())
+
+  log.Printf(" |  Writing Dockerfile.")
+  tmpl, err = template.New("dockerfile").Parse(dockerfileTemplate)
+  if err != nil {
+    return err
+  }
+  if err := tmpl.Execute(dockerfile, templateCtx); err != nil {
+    return err
+  }
+  log.Printf("    Done.")
+
+  log.Printf(" |  Building container.")
+  imageName := fmt.Sprintf("plumb/%s", ctx.Name)
+  cmd := exec.Command("docker", "build", "-t", imageName, "-f", dockerfile.Name(), path)
+
+  stdout, err := cmd.StdoutPipe()
+  if err != nil {
+    return err
+  }
+
+  stderr, err := cmd.StderrPipe()
+  if err != nil {
+    return err
+  }
+
+  if err := cmd.Start(); err != nil {
+    return err
+  }
+
+  multi := io.MultiReader(stdout, stderr)
+  in := bufio.NewScanner(multi)
+
+  for in.Scan() {
+    log.Printf("    %s", in.Text())
+  }
+  if err := in.Err(); err != nil {
+    log.Printf("    error: %s", err)
+  }
+
+  err = cmd.Wait()
+  if err != nil {
+    return err
+  }
+
+  log.Printf("    Container 'plumb/%s' built.", ctx.Name)
+  return nil
+}
