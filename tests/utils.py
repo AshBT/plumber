7433ee644bb2c52000414be4af0ca8b8311ea11a
Eric Chu <eytchu@gmail.com>
2015-05-27 16:39:50 -0700

    changes the way tests are run
        the test db is populated a single row at a time and the
        enhancer run over this single node; its output is checked
    
        this lets us print logs and stdout properly on test failures

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,5 @@
 from py2neo import Node
-
-def check_equals(x, y):
-    assert x == y
+import nose.tools
 
 def check_exists(x, attribute):
     try:
@@ -15,49 +13,52 @@ def check_not_exists(x, attribute):
     except KeyError:
         assert True, "key {} unexpectedly exists".format(attribute)
 
-def check_ad_equals_expected(ad, expected, ignore_fields=[]):
-    # check that all expected keys are present
-    for key in expected:
-        assert (ad.properties[key] is not None), ("{} is missing".format(key))
-
-    for key in ad.properties:
-        a = ad[key]
-        check_exists(expected, key)
-        b = expected[key]
-        if key in ignore_fields:
-            continue
-        if isinstance(a, list):
-            a.sort()
-        if isinstance(b, list):
-            b.sort()
-        check_equals(a, b)
-
 class Runner(object):
-    @classmethod
-    def setup_class(cls, EnhancerToTest, test_nodes):
-        cls.ENHANCER = EnhancerToTest()
-        # seed some data
-        for node in test_nodes:
-            cls.ENHANCER.db.create(Node("Ad", **node))
+    # subclasses have to set these fields
+    ENHANCER = None
+    OUTPUT = []
+    INPUT = []
+    IGNORE = [] # this is optional, setting a list of IGNORE
+                # first checks that the fields exist before ignoring
+                # their values. This is useful for instance, when
+                # comparing "tweets" which may change over time.
 
-    @classmethod
-    def teardown_class(cls):
-        cls.ENHANCER.db.delete_all()
+    def check_ad_equals_expected(self, test_node, expected, ignore_fields=[]):
+        try:
+            enhancer = self.ENHANCER()
+            enhancer.db.create(Node("Ad", **test_node))
 
-    def test_run(self, expected_results, ignore_fields=[]):
-        """
-        Setting a list of ignore_fields first checks that the fields
-        exist before ignoring their values. This is useful for instance,
-        when comparing "tweets" which may change over time.
-        """
-        self.ENHANCER.run(skip_on_error=False)
+            enhancer.run(skip_on_error=False)
 
-        # use the ENHANCER's DB to check that results are as expected
-        records = self.ENHANCER.db.cypher.execute("MATCH (ad) RETURN ad")
+            # use the ENHANCER's DB to check that results are as expected
+            records = enhancer.db.cypher.execute("MATCH (ad) RETURN ad")
 
-        # first, check that we haven't *added* any records
-        yield check_equals, len(records), len(expected_results)
+            assert len(records)==1, "We're expecting only one entry in the DB"
 
-        # second, check that the records contain the expected fields
-        for result, exp in zip(records, expected_results):
-            yield check_ad_equals_expected, result.ad, exp, ignore_fields
+            ad = records[0].ad
+
+            # check that all expected keys are present
+            for key in expected:
+                assert (ad.properties[key] is not None), ("{} is missing".format(key))
+
+            for key in ad.properties:
+                a = ad[key]
+                check_exists(expected, key)
+                b = expected[key]
+                if key in ignore_fields:
+                    continue
+                if isinstance(a, list):
+                    a.sort()
+                if isinstance(b, list):
+                    b.sort()
+                nose.tools.eq_(a, b)
+        finally:
+            enhancer.db.delete_all()
+
+    def test_run(self):
+        # Populates the DB one by one and runs the enhancer on it.
+        # This structure lets us see captured log outputs.
+
+        # check that the records contain the expected fields
+        for test_node, exp in zip(self.INPUT, self.OUTPUT):
+            yield self.check_ad_equals_expected, test_node, exp, self.IGNORE
