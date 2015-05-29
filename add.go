15f6394b5c9c91194a826b7891951668a8801be7
Eric Chu <eytchu@gmail.com>
2015-05-28 21:40:18 -0700

    added 'add' functionality
        gives users the ability to add a bundle to a pipeline

---
+++ add.go
@@ -0,0 +1,59 @@
+package plumb
+
+import (
+	"fmt"
+	"log"
+	"github.com/qadium/plumb/shell"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+func addOne(pipeline string, bundle string) error {
+	log.Printf(" |  Adding '%s' to '%s'.", bundle, pipeline)
+	defer log.Printf("    Added '%s'.", bundle)
+
+	path, err := pipelinePath(pipeline)
+	if err != nil {
+		return err
+	}
+
+	log.Printf(" |  Parsing bundle config.")
+	ctx, err := parseConfig(bundle)
+	log.Printf("    Done.")
+
+	log.Printf(" |  Copying `.plumb.yml` config to `%s.yml`.", ctx.Name)
+	config := fmt.Sprintf("%s/%s.yml", path, ctx.Name)
+	bytes, err := yaml.Marshal(&ctx)
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(config, bytes, 0544); err != nil {
+		return err
+	}
+	log.Printf("    Done.")
+
+	log.Printf(" |  Adding `%s.yml` to version control.", ctx.Name)
+
+	if err := shell.RunAndLog("git", "-C", path, "add", config ); err != nil {
+		return err
+	}
+
+	message := fmt.Sprintf("Updated '%s' config.", ctx.Name)
+	if err := shell.RunAndLog("git", "-C", path, "commit", "-m", message, "--author", "\"Plumb Bot <plumb@qadium.com>\""); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Add(pipeline string, bundles ...string) error {
+	log.Printf("==> Adding '%v' to '%s' pipeline", bundles, pipeline)
+	defer log.Printf("<== Adding complete.")
+
+	for _, bundle := range bundles {
+		addOne(pipeline, bundle)
+	}
+
+	return nil
+}
