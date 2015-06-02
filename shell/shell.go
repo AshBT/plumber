2e598a4ff0269787624c8f4ef72773e735032b5f
Eric Chu <eytchu@gmail.com>
2015-06-02 14:49:07 -0700

    updated shell
        stdout and stderr have different formats
        interrupt signal sent to subprocess

--- shell/shell.go
+++ shell/shell.go
@@ -2,15 +2,40 @@ package shell
 
 import (
 	"os/exec"
+	"os/signal"
+	"os"
 	"log"
 	"bufio"
 	"io"
 	"strings"
 )
 
+func logBuffer(prefix string, pipe io.ReadCloser) {
+	in := bufio.NewScanner(pipe)
+
+	for in.Scan() {
+		log.Printf("    %s %s", prefix, in.Text())
+	}
+	if err := in.Err(); err != nil {
+		log.Printf("    error %s", err)
+	}
+}
+
 func RunAndLog(name string, args ...string) error {
 	log.Printf("    Exec shell: '%s'", name + " " + strings.Join(args, " "))
 	cmd := exec.Command(name, args...)
+
+	// install signal handler to forward interrupts to subprocess
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	defer signal.Stop(sig)
+
+	go func() {
+		// wait for signal from CTRL-C
+		<- sig
+		log.Printf("    Received CTRL-C; terminating '%s' process.", name)
+		cmd.Process.Signal(os.Interrupt)
+	}()
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -26,15 +51,8 @@ func RunAndLog(name string, args ...string) error {
 		return err
 	}
 
-	multi := io.MultiReader(stdout, stderr)
-	in := bufio.NewScanner(multi)
-
-	for in.Scan() {
-		log.Printf("    > %s", in.Text())
-	}
-	if err := in.Err(); err != nil {
-		log.Printf("    > error: %s", err)
-	}
+	go logBuffer(">", stdout)
+	go logBuffer("!", stderr)
 
 	err = cmd.Wait()
 	if err != nil {
