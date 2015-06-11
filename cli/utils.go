994b385e6e80f6946fd526b48a5d52b2e010b7bf
Eric Chu <eytchu@gmail.com>
2015-06-10 19:11:11 -0700

    updated utils
        - defined a Context struct that carries auxiliary info
        - modified tests to reflect this change

--- cli/utils.go
+++ cli/utils.go
@@ -6,28 +6,57 @@ import (
 	"os/user"
 )
 
+// Useful information for the plumber CLI tool goes here
+type Context struct {
+	PipeDir 	string	// the directory to store plumber pipelines
+	KubeSuffix 	string	// the suffix to use to store kubernetes files
+	GitCommit	string	// the current git commit
+	Version		string 	// the current version
+	Image		string	// the desired image name
+	BootstrapDir string	// the directory to use for bootstrapping
+}
+
 const plumberDir = ".plumber"
+
+const bootstrapDir = ".plumber-bootstrap"
 
 const k8sDir = "k8s"
 
-// Given the `name` of a pipeline, return the path where we should store
-// information about it.
-func PipelinePath(name string) (string, error) {
+// The default context stores all plumber pipelines in the user's
+// home directory at ~/.plumber; all kubernetes files are stored at
+// ~/.plumber/$PIPELINE/k8s
+//
+// It also includes some basic versioning information
+func NewDefaultContext() (*Context, error) {
+	// use the current user to store the plumb data
 	usr, err := user.Current()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	path := fmt.Sprintf("%s/%s/%s", usr.HomeDir, plumberDir, name)
-	return path, nil
+
+	d := &Context{
+		fmt.Sprintf("%s/%s", usr.HomeDir, plumberDir),
+		k8sDir,
+		GitCommit,
+		versionString(),
+		"plumber/manager",
+		fmt.Sprintf("%s/%s", usr.HomeDir, bootstrapDir),
+	}
+	return d, nil
+}
+
+
+// Given the `name` of a pipeline, return the path where we should store
+// information about it.
+func (d *Context) PipelinePath(name string) string {
+	return fmt.Sprintf("%s/%s", d.PipeDir, name)
 }
 
 // Get a pipeline; this differs from PipelinePath in that it also checks
 // if the file / path exists.
-func GetPipeline(name string) (string, error) {
-	path, err := PipelinePath(name)
-	if err != nil {
-		return "", err
-	}
+func (d *Context) GetPipeline(name string) (string, error) {
+	path := d.PipelinePath(name)
+
 	// make sure file exists and we have permissions, etc.
 	if _, err := os.Stat(path); err != nil {
 		return "", err
@@ -37,11 +66,16 @@ func GetPipeline(name string) (string, error) {
 
 // Given the `name` of a pipeline, return the path where we should store
 // kubernetes YAML files for pods, services, and replication controllers
-func KubernetesPath(name string) (string, error) {
-	path, err := PipelinePath(name)
-	if err != nil {
-		return "", err
+func (d *Context) KubernetesPath(name string) string {
+	path := d.PipelinePath(name)
+	k8s := fmt.Sprintf("%s/%s", path, d.KubeSuffix)
+	return k8s
+}
+
+func versionString() string {
+	versionString := version
+	if versionPrerelease != "" {
+		versionString += "-" + versionPrerelease
 	}
-	k8s := fmt.Sprintf("%s/%s", path, k8sDir)
-	return k8s, nil
+	return versionString
 }
--- cli/utils_test.go
+++ cli/utils_test.go
@@ -6,31 +6,65 @@ import (
 	"os"
 	"os/user"
 	"testing"
+	"io/ioutil"
 )
 
-func TestPipelinePath(t *testing.T) {
+const testPlumberDir = "foo"
+
+const testBootstrapDir = "boot"
+
+const testKubeSuffix = "k8s"
+
+// mock for cli context (used for testing)
+// uses temp directories
+func NewTestContext(t *testing.T) (*cli.Context, string) {
+	// use the current user to store the plumb data
 	usr, err := user.Current()
 	if err != nil {
-		t.Error("Could not get the user running this test. /shrug")
+		t.Errorf("Got an error getting current user: '%v'", err)
 	}
 
-	expectedPath := fmt.Sprintf("%s/.plumber/foobar", usr.HomeDir)
+	tempDir, err := ioutil.TempDir(usr.HomeDir, "plumberTest")
+	if err != nil {
+		t.Errorf("Got an error constructing context: '%v'", err)
+	}
 
-	path, err := cli.PipelinePath("foobar")
+	d := &cli.Context{
+		fmt.Sprintf("%s/%s", tempDir, testPlumberDir),
+		testKubeSuffix,
+		"",
+		"test-version",
+		"plumber/test-manager",
+		fmt.Sprintf("%s/%s", tempDir, testBootstrapDir),
+	}
+	return d, tempDir
+}
+
+func cleanTestDir(t *testing.T, tempDir string) {
+	if err := os.RemoveAll(tempDir); err != nil {
+		t.Errorf("Had an issue removing the temp file, '%v'", err)
+	}
+}
+
+func TestPipelinePath(t *testing.T) {
+	ctx, tempDir := NewTestContext(t)
+	defer cleanTestDir(t, tempDir)
+
+	expectedPath := fmt.Sprintf("%s/foobar", ctx.PipeDir)
+
+	path := ctx.PipelinePath("foobar")
 	if expectedPath != path {
 		t.Error("PipelinePath: did not return expected path.")
 	}
 }
 
 func TestGetPipeline(t *testing.T) {
-	usr, err := user.Current()
-	if err != nil {
-		t.Error("Could not get the user running this test. /shrug")
-	}
+	ctx, tempDir := NewTestContext(t)
+	defer cleanTestDir(t, tempDir)
 
-	expectedPath := fmt.Sprintf("%s/.plumber/you-better-hope-nobody-names-pipelines-this-way", usr.HomeDir)
+	expectedPath := fmt.Sprintf("%s/mypipe", ctx.PipeDir)
 	// first, check that we fail with a "no such directory"
-	path, err := cli.GetPipeline("you-better-hope-nobody-names-pipelines-this-way")
+	path, err := ctx.GetPipeline("mypipe")
 	if err == nil || err.Error() != fmt.Sprintf("stat %s: no such file or directory", expectedPath) {
 		t.Errorf("We expected to fail with an error with no such directory, but got '%v' instead", err)
 	}
@@ -40,21 +74,19 @@ func TestGetPipeline(t *testing.T) {
 	}
 	defer os.RemoveAll(expectedPath)
 
-	path, err = cli.GetPipeline("you-better-hope-nobody-names-pipelines-this-way")
+	path, _ = ctx.GetPipeline("mypipe")
 	if path != expectedPath {
 		t.Error("GetPipeline: did not return expected path.")
 	}
 }
 
 func TestKubernetesPath(t *testing.T) {
-	usr, err := user.Current()
-	if err != nil {
-		t.Error("Could not get the user running this test. /shrug")
-	}
+	ctx, tempDir := NewTestContext(t)
+	defer cleanTestDir(t, tempDir)
 
-	expectedPath := fmt.Sprintf("%s/.plumber/barbaz/k8s", usr.HomeDir)
+	expectedPath := fmt.Sprintf("%s/barbaz/k8s", ctx.PipeDir)
 
-	path, err := cli.KubernetesPath("barbaz")
+	path := ctx.KubernetesPath("barbaz")
 	if expectedPath != path {
 		t.Error("KubernetesPath: did not return expected path.")
 	}
