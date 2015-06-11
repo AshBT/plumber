55f34e3cea1a71aba4879ee34cfee2d787f5c907
Eric Chu <eytchu@gmail.com>
2015-06-10 22:04:22 -0700

    added tests for create command

---
+++ cli/create_test.go
@@ -0,0 +1,73 @@
+package cli_test
+
+import (
+	"testing"
+	"fmt"
+	"os"
+	"io/ioutil"
+	"log"
+)
+
+func TestCreate(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+
+	ctx, tempDir := NewTestContext(t)
+	defer cleanTestDir(t, tempDir)
+
+	// create the pipeline
+	if err := ctx.Create("foobar"); err != nil {
+		t.Errorf("TestCreate: '%v'", err)
+	}
+
+	project := fmt.Sprintf("%s/foobar", ctx.PipeDir)
+	// check 1. directory exists
+	if _, err := os.Stat(project); err != nil {
+		t.Errorf("TestCreate: directory didn't get created, '%v'", err)
+	}
+
+	// check 2. it has git initialized (check for .git directory)
+	if _, err := os.Stat(fmt.Sprintf("%s/.git", project)); err != nil {
+		t.Errorf("TestCreate: directory did not have git initialized, '%v'", err)
+	}
+}
+
+// when passing an empty string, should give an error
+func TestEmptyCreate(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+
+	ctx, tempDir := NewTestContext(t)
+	defer cleanTestDir(t, tempDir)
+
+	// create the pipeline
+	if err := ctx.Create(""); err == nil || err.Error() != "Cannot create a pipeline with no name." {
+		t.Errorf("TestEmptyCreate did not fail.")
+	}
+}
+
+// when creating an existing pipeline, should give an error
+func TestExistingCreate(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+
+	ctx, tempDir := NewTestContext(t)
+	defer cleanTestDir(t, tempDir)
+
+	// create the pipeline
+	if err := ctx.Create("foobar"); err != nil {
+		t.Errorf("TestExistingCreate: '%v'", err)
+	}
+
+	// create it again! this time, it should fail
+	if err := ctx.Create("foobar"); err == nil || err.Error() != "Pipeline already exists." {
+		t.Errorf("TestingExistingCreate did not detect already existing pipeline.")
+	}
+
+}
+// path, err := PipelinePath(name)
+// if err != nil {
+// 	return "", err
+// }
+// // make sure file exists and we have permissions, etc.
+// if _, err := os.Stat(path); err != nil {
+// 	return "", err
+// }
+// return path, nil
