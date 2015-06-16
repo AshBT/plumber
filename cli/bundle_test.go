b431ef57f38125c71ececc5895f0750965c67c04
Eric Chu <eytchu@gmail.com>
2015-06-16 11:54:41 -0700

    utility function for creating bundles under test

--- cli/bundle_test.go
+++ cli/bundle_test.go
@@ -11,22 +11,6 @@ import (
 	"bytes"
 )
 
-// put bundle under test by inputting yaml from string and checking
-// that output Dockerfile is expected
-//
-// would be nice to run the container and curl it, but that is also
-// checked manually
-//
-// we manually check that output Dockerfile can be built
-//
-// so we're just checking that *strings* match, but not that the
-// functionality is as expected
-//
-// we probably shouldn't test the wrappers anyway, since they should be
-// tested in a separate repo (so we can have language agnosticism)
-
-// We'll check that the Bundle command actually works
-
 // TODO: this test code should live in the language-dependent repository
 // and our test should *load* those tests
 //
@@ -37,20 +21,26 @@ def run(in_dict):
 	return in_dict
 `
 
-func TestBundle(t *testing.T) {
-	//log.SetOutput(ioutil.Discard)
+const bundleFormat = `
+language: python
+name: %s
+inputs:
+  - name: a
+    type: string
+outputs:
+  - name: b
+    type: string
+extra: field`
 
-	ctx, tempDir := NewTestContext(t)
-	defer cleanTestDir(t, tempDir)
-
-	// use the `goodBundle` from config_test.go
+func createTestBundle(t *testing.T, bundleName, tempDir string) {
 	filename := fmt.Sprintf("%s/.plumb.yml", tempDir)
-	if err := ioutil.WriteFile(filename, []byte(optBundle), 0644); err != nil {
+	bundle := fmt.Sprintf(bundleFormat, bundleName)
+	if err := ioutil.WriteFile(filename, []byte(bundle), 0644); err != nil {
 		t.Errorf("Could not write .plumb.yml; got error '%v'", err)
 	}
 
 	// write out the python test code
-	filename = fmt.Sprintf("%s/foobar.py", tempDir)
+	filename = fmt.Sprintf("%s/%s.py", tempDir, bundleName)
 	if err := ioutil.WriteFile(filename, []byte(bundleTestPython), 0644); err != nil {
 		t.Errorf("Could not write foobar.py; got error '%v'", err)
 	}
@@ -60,6 +50,16 @@ func TestBundle(t *testing.T) {
 	if err := ioutil.WriteFile(filename, []byte{}, 0644); err != nil {
 		t.Errorf("Could not write requirements.txt; got error '%v'", err)
 	}
+}
+
+func TestBundle(t *testing.T) {
+	//log.SetOutput(ioutil.Discard)
+
+	ctx, tempDir := NewTestContext(t)
+	defer cleanTestDir(t, tempDir)
+
+	// create a bundle for testing
+	createTestBundle(t, "foobar", tempDir)
 
 	// bundle the tempDir
 	err := ctx.Bundle(tempDir)
