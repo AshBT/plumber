f00714421171359877ce7762bb92bcb514bbe8e9
Eric Chu <eytchu@gmail.com>
2015-06-29 20:58:47 -0700

    updated add command
        - now reports skipped directories
        - summarizes skipped directories (if any)

--- cli/add.go
+++ cli/add.go
@@ -16,37 +16,24 @@
 package cli
 
 import (
-	"os"
 	"fmt"
 	"github.com/qadium/plumber/shell"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"os"
 )
 
-func addOne(ctx *Context, pipeline string, bundle string) error {
-	log.Printf(" |  Adding '%s' to '%s'.", bundle, pipeline)
-	defer log.Printf("    Processed '%s'.", bundle)
-
-	path, err := ctx.GetPipeline(pipeline)
-	if err != nil {
-		return err
-	}
-
+func addOne(ctx *Context, pipelinePath string, bundle string) error {
 	log.Printf(" |  Parsing bundle config.")
 	bundleCfg, err := ParseBundleFromDir(bundle)
 	if err != nil {
-		if os.IsNotExist(err) {
-			log.Printf("    Could not find '%s' in path '%s'.", bundleConfig, bundle)
-			log.Printf("    Skipping '%s'.", bundle)
-			return nil
-		}
 		return err
 	}
 	log.Printf("    Done.")
 
 	log.Printf(" |  Copying `.plumber.yml` config to `%s.yml`.", bundleCfg.Name)
-	config := fmt.Sprintf("%s/%s.yml", path, bundleCfg.Name)
+	config := fmt.Sprintf("%s/%s.yml", pipelinePath, bundleCfg.Name)
 	bytes, err := yaml.Marshal(&bundleCfg)
 	if err != nil {
 		return err
@@ -59,12 +46,12 @@ func addOne(ctx *Context, pipeline string, bundle string) error {
 
 	log.Printf(" |  Adding `%s.yml` to version control.", bundleCfg.Name)
 
-	if err := shell.RunAndLog("git", "-C", path, "add", config); err != nil {
+	if err := shell.RunAndLog("git", "-C", pipelinePath, "add", config); err != nil {
 		return err
 	}
 
 	message := fmt.Sprintf("Updated '%s' config.", bundleCfg.Name)
-	if err := shell.RunAndLog("git", "-C", path, "commit", "-m", message, "--author", "\"Plumber Bot <plumber@qadium.com>\""); err != nil {
+	if err := shell.RunAndLog("git", "-C", pipelinePath, "commit", "-m", message, "--author", "\"Plumber Bot <plumber@qadium.com>\""); err != nil {
 		return err
 	}
 
@@ -75,11 +62,29 @@ func (ctx *Context) Add(pipeline string, bundles ...string) error {
 	log.Printf("==> Adding '%v' to '%s' pipeline", bundles, pipeline)
 	defer log.Printf("<== Adding complete.")
 
+	path, err := ctx.GetPipeline(pipeline)
+	if err != nil {
+		return err
+	}
+
+	skipped := []string{}
 	for _, bundle := range bundles {
-		if err := addOne(ctx, pipeline, bundle); err != nil {
-			return err
+		log.Printf(" |  Adding '%s' to '%s'.", bundle, pipeline)
+		if err := addOne(ctx, path, bundle); err != nil {
+			if os.IsNotExist(err) {
+				log.Printf("    Could not find '%s' in path '%s'.", bundleConfig, bundle)
+				log.Printf("    Skipped '%s'.", bundle)
+				skipped = append(skipped, bundle)
+			} else {
+				return err
+			}
+		} else {
+			log.Printf("    Added '%s'.", bundle)
 		}
 	}
 
+	if len(skipped) > 0 {
+		log.Printf(" *   Skipped '%v'", skipped)
+	}
 	return nil
 }
